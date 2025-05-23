41d7993e8cd8a0493c7ed55a6ffb0601111e1bb5
Kirya <kkodolov40@gmail.com>
2025-05-23 19:16:32 +0300

    Добавлены куки файлы для языка, теперь он сохраняется даже после перезапуска

--- src/main/java/org/kirya343/config/LocalisationConfig.java
+++ src/main/java/org/kirya343/config/LocalisationConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -25,15 +25,17 @@ public class LocalisationConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver resolver = new SessionLocaleResolver();
-        resolver.setDefaultLocale(Locale.forLanguageTag("ru")); // По умолчанию русский
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.forLanguageTag("ru")); // Язык по умолчанию
+        resolver.setCookieName("locale"); // Название cookie
+        resolver.setCookieMaxAge(60 * 60 * 24 * 30); // Срок действия: 30 дней
         return resolver;
     }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-        interceptor.setParamName("lang"); // ?lang=fi
+        interceptor.setParamName("lang"); // Параметр в URL, например: ?lang=fi
         return interceptor;
     }
 
