188ab79a26b48f9897db1487eef71fbd37c1070a
Kirya <kkodolov40@gmail.com>
2025-07-04 03:19:49 +0300

    Кое как склепал страницу

--- src/main/java/org/kirya343/main/controller/AdminTasksController.java
+++ src/main/java/org/kirya343/main/controller/AdminTasksController.java
@@ -1,5 +1,12 @@
 package org.kirya343.main.controller;
 
+import java.util.List;
+
+import org.kirya343.main.model.Task;
+import org.kirya343.main.model.User;
+import org.kirya343.main.model.User.Role;
+import org.kirya343.main.repository.TaskRepository;
+import org.kirya343.main.repository.UserRepository;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
@@ -14,9 +21,16 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/admin/tasks")
 public class AdminTasksController {
 
+    private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
+
     @GetMapping
     public String adminTasks(Model model, @AuthenticationPrincipal OAuth2User oAuth2User) {
-        
+        List<Task> tasks = taskRepository.findAll();
+        List<User> executors = userRepository.findByRole(Role.ADMIN);
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("executors", executors);
+        model.addAttribute("activePage", "admin-tasks");
         return "admin/tasks";
     }
 }
--- src/main/java/org/kirya343/main/repository/UserRepository.java
+++ src/main/java/org/kirya343/main/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package org.kirya343.main.repository;
 
 import org.kirya343.main.model.User;
+import org.kirya343.main.model.User.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -20,4 +22,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     long countActiveUsers();
 
     Page<User> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    List<User> findByRole(Role role);
 }
